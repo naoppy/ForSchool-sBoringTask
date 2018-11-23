a0d393fb3976eb251fbe33e7b9c73c27c544962c
Naoki Otani <e1710@s.akashi.ac.jp>
2018-11-23 22:52:06 +0900

    changed more friendly

--- PlotComplex.py
+++ PlotComplex.py
@@ -1,15 +1,20 @@
-import cv2
+﻿import cv2
 import numpy as np
 
-scale = 100
-size = 200
+# ====setting====
+scale = 30
+windowSize = 600
+# ===============
 
-img = np.full((2 * size, 2 * size, 3), 128, dtype=np.uint8)
+size = int(windowSize/2)
+img = np.full((windowSize, windowSize, 3), 128, dtype=np.uint8)
 
 
 def w(point):
     # y座標の向きが逆なので戻す
     point[1] *= -1
+
+    print("{}{:+}j".format(point[0], point[1]))
 
     point[0] *= scale
     point[0] = int(point[0])
@@ -19,18 +24,20 @@ def w(point):
     point[0] += size
     point[1] += size
     point = tuple(point)
-    print(point)
     cv2.arrowedLine(img, (size, size), point, (0, 0, 0), thickness=2)
 
 
 # x軸とy軸
-cv2.line(img, (size, 0), (size, 2 * size), (255, 0, 0), thickness=1)
-cv2.line(img, (0, size), (2 * size, size), (255, 0, 0), thickness=1)
+cv2.line(img, (size, 0), (size, windowSize), (255, 0, 0), thickness=1)
+cv2.line(img, (0, size), (windowSize, size), (255, 0, 0), thickness=1)
 
-w([32/5, 24/5])
-w([1.28, 0.96])
-w([1.20, -1.60])
-w([-0.48, 0.64])
+# ====plotData====
+w([2 / 5, -1 / 5])
+w([8, -4])
+w([4, 8])
+w([-2, -4])
+w([10, 0])
+# ================
 
 cv2.imwrite('opencv_draw_argument.png', img)
 
